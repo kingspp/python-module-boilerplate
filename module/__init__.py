20f12cbe03a662a818b5befd577219985277a5f8
kingspp <kingspprathyush@gmail.com>
2017-11-20 16:14:07 +0530

    Updated parent module initialization functions

--- module/__init__.py
+++ module/__init__.py
@@ -1,4 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 20/05/16,
+| **@author:** Prathyush SP,
+| **@version:** v0.0.1
+|
+| **Description:**
+| Singleton Class
+|
+| Sphinx Documentation Status:** Complete
+|
+..todo::
+"""
+import os
+from module.logging_config_manager import setup_logging
 
-def main():
-    """Entry point for the application script"""
-    print("Call your main application code here")
+
+class Singleton(type):
+    """
+    Singleton Class
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+# todo: Prathyush SP: Move logging config to logging manager
+
+setup_logging(default_path=os.path.join("/".join(__file__.split('/')[:-1]), 'config', 'module_logging.yaml'))
+
+# todo: Prathyush SP - Debug code breakages due to changing start method
+# import multiprocessing
+# multiprocessing.context._force_start_method('spawn')
+
+from module.store import MODULE_STORE
+from module.config_manager import MODULE_CONFIG, MODULE_CONFIG_DATA
+from module.metadata import metadata as md
+
+__version__ = md.__version__
+
+if __name__ == '__main__':
+    print('module.__init__ success . . .')
