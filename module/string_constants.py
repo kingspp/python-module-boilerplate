d1952ef5657510705a6912d9c2452d42e0eff8c6
kingspp <kingspprathyush@gmail.com>
2017-11-20 16:07:30 +0530

    Added string constants structure

---
+++ module/string_constants.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 17/11/17,
+| **@author:** prathyushsp,
+| **@version:** v0.0.1
+|
+| **Description:**
+| 
+|
+| **Sphinx Documentation Status:** Complete
+|
+..todo::
+    --
+"""
+import enum
+from enum import auto
+
+
+class SampleConstant(enum):
+    """"
+    | **@author:** Prathyush SP,
+    |
+    | Sample Constants
+    """
+    CONSTANT_1 = auto()
+    CONSTANT_2 = auto()
+    CONSTANT_3 = auto()
+    CONSTANT_4 = auto()
