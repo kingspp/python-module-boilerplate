152960092bb5956545be69c8a14fbeacb33e026e
Prathyush SP <kingspprathyush@gmail.com>
2018-06-30 15:38:20 +0530

    [EN] - Updated module to fetch metadata from json.

--- module/metadata.py
+++ module/metadata.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 29/06/18,
+| **@author:** prathyushsp,
+| **@version:** v0.0.1
+|
+| **Description:**
+|
+|
+| **Sphinx Documentation Status:** --
+|
+..todo::
+"""
+
 import json
-import bunch
-metadata = bunch.Bunch(json.load(open('/'.join(str(__file__).split('/')[:-1])+'/metadata.json')))
+import munch
+
+metadata = munch.Munch(json.load(open('/'.join(str(__file__).split('/')[:-1]) + '/metadata.json')))
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+from module.metadata import metadata as md
 
 here = path.abspath(path.dirname(__file__))
 
@@ -18,53 +19,31 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='module',
+    name=md.__name__,
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version=md.__version__,
 
-    description='A module Python project',
+    description=md.__description__,
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url=md.__url__,
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author=md.__author__,
+    author_email=md.__email__,
 
     # Choose your license
-    license='MIT',
+    license=md.__license__,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+    classifiers=md.__classifiers__,
 
     # What does your project relate to?
-    keywords='module setuptools development',
+    keywords=md.__keywords__,
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -78,7 +57,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[i.replace('\n','') for i in open('requirements.txt', 'r').readlines()],
+    install_requires=[i.replace('\n', '') for i in open('requirements.txt', 'r').readlines()],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
